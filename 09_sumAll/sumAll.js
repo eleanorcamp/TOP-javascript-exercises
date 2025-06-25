c69ee3af665cf8d0467dd396fc6a1a3132d0827e
eleanorcamp <elliejcamp@gmail.com>
2025-06-25 15:28:31 -0400

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0){ return "ERROR" }
+    if (second < 0){ return "ERROR" }
+    if (!Number.isInteger(first)){ return "ERROR" }
+    if (!Number.isInteger(second)){ return "ERROR" }
+    
+    let sum = 0;
+    let lower, upper;
+
+    if (first < second){
+        lower = first, upper = second
+    } else{
+        lower = second, upper = first
+    }
+
+    for (let i = lower; i<=upper; i++){
+        sum += i
+    }
+    return sum;
 
 };
 
